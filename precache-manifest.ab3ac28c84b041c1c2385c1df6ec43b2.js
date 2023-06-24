5f7dc87084df162867e7661ba1e77fa53d0daada
harsh790608 <yash9997771652@gmail.com>
2023-06-25 01:07:12 +0530

    Updates

---
+++ precache-manifest.ab3ac28c84b041c1c2385c1df6ec43b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "015428b82a7d08f1c636",
+    "url": "/newsmonkey/static/css/main.5fea63df.chunk.css"
+  },
+  {
+    "revision": "015428b82a7d08f1c636",
+    "url": "/newsmonkey/static/js/main.015428b8.chunk.js"
+  },
+  {
+    "revision": "8a1229037873ab4e6605",
+    "url": "/newsmonkey/static/js/1.8a122903.chunk.js"
+  },
+  {
+    "revision": "92549d672139342ee5ee",
+    "url": "/newsmonkey/static/js/2.92549d67.chunk.js"
+  },
+  {
+    "revision": "d87b788b8a2d4b59e162",
+    "url": "/newsmonkey/static/js/runtime~main.d87b788b.js"
+  },
+  {
+    "revision": "c811092b36b7984bdc7b1e92cf6926b9",
+    "url": "/newsmonkey/index.html"
+  }
+];
